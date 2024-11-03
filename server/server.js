f5ba18b691a357ab22ab0aa33c8aa16df8d01f99
lmvinh <amis.lmv1304@gmail.com>
2024-11-03 18:24:36 +0700

    11

--- server/server.js
+++ server/server.js
@@ -1,72 +1,64 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const lyricsFinder = require("lyrics-finder");
-const SpotifyWebApi = require("spotify-web-api-node");
+//equire("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const lyricsFinder = require("lyrics-finder")
+const SpotifyWebApi = require("spotify-web-api-node")
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// Refresh Token Endpoint
 app.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken;
+  const refreshToken = req.body.refreshToken
   const spotifyApi = new SpotifyWebApi({
     redirectUri: "http://localhost:3000",
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientId: "ef21180efe0a4fc8978edb0e875d9af2",
+    clientSecret: "dfd88b4b5ff94eb9aa4895884c22fa00",
     refreshToken,
-  });
+  })
 
   spotifyApi
     .refreshAccessToken()
-    .then((data) => {
+    .then(data => {
       res.json({
-        accessToken: data.body.access_token,
-        expiresIn: data.body.expires_in,
-      });
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      })
     })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(400);
-    });
-});
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
 
-// Login Endpoint
 app.post("/login", (req, res) => {
-  const code = req.body.code;
+  const code = req.body.code
   const spotifyApi = new SpotifyWebApi({
     redirectUri: "http://localhost:3000",
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  });
+    clientId: "ef21180efe0a4fc8978edb0e875d9af2",
+    clientSecret: "dfd88b4b5ff94eb9aa4895884c22fa00",
+  })
 
   spotifyApi
     .authorizationCodeGrant(code)
-    .then((data) => {
+    .then(data => {
       res.json({
         accessToken: data.body.access_token,
         refreshToken: data.body.refresh_token,
         expiresIn: data.body.expires_in,
-      });
+      })
     })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(400);
-    });
-});
+    .catch(err => {
+      res.sendStatus(400)
+    })
+})
 
-// Lyrics Endpoint
 app.get("/lyrics", async (req, res) => {
-  try {
-    const lyrics =
-      (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found";
-    res.json({ lyrics });
-  } catch (error) {
-    console.error(error);
-    res.status(400).send("Error fetching lyrics");
-  }
-});
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
+  res.json({ lyrics })
+})
 
-module.exports = app; // Export the app for serverless handling
+app.listen(3001)
