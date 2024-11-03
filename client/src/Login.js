ce4de867ac77b1485b2e4aa8b41d826caebea843
lmvinh <amis.lmv1304@gmail.com>
2024-11-03 21:08:43 +0700

    123

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,7 +1,7 @@
+import React from "react";
 
-import React from "react"
-const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=ef21180efe0a4fc8978edb0e875d9af2&response_type=code&redirect_uri=http://music-6bh7.vercel.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = 
+  "https://accounts.spotify.com/authorize?client_id=ef21180efe0a4fc8978edb0e875d9af2&response_type=code&redirect_uri=https://music-6bh7.vercel.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
   return (
@@ -13,5 +13,5 @@ export default function Login() {
         Login With Spotify
       </a>
     </div>
-  )
+  );
 }
